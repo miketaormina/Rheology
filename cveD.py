39017b62674d39dabc955f244c342408e162f00e
Mike Taormina <mtaormin@uoregon.edu>
2014-09-09 10:43:23 -0700

    Added some comments to the module. calling help(cve.foo) now returns info about function foo
    Fixed a bug that left out the scale factor in cveD.cveD

--- cveD.py
+++ cveD.py
@@ -1,3 +1,15 @@
+"""A module for analyzing diffusion data, calculating D and verifying free difusion behavior as in Vestergaard 2014.
+
+Functions:
+getData loads data from a .mat file into a scipy object.
+cveD calculates the covariance-based estimator for the diffusion coefficient and localization variance. It also returns the variance in D, signal to noise ratio, and the computed power spectrum of displacement values for use with gammaChiSquare function.
+theoryP computes the theoretical power spectrum for a freely diffusing particle.
+boxAv computes a box averaged array, useful for comparing the power spectrum to it's theoretical value.
+gammaChiSquare compares an inpyt numpy array to a gamma distribution of shape 1/2 and scale 2 and returns a p value and reduced chi^2 value. Useful for verifying free difusion.
+D2eta computes a viscosity in Pa*s for a D in um^2/s, R in um, and T in Celcius
+"""
+
+
 import numpy as np
 pi = np.pi
 import matplotlib.pyplot as plt
@@ -8,22 +20,37 @@ import scipy.constants as const
 
 #%matplotlib tk
 
+# References: [1] Optimal estimation of diffusion coefficients from single-particle trajectories. Vestergaard, et al PRE (2014)
+
 def getData(filename):
+	"""Returns data from a .mat file"""
 	#Filename is a string and indicates the path to a matlab file containing particle track info in objs_link (from RP tracking GUI)
 	data = scipy.io.loadmat(filename, matlab_compatible=True)
 	return data
 
 def cveD(x, dt, scale):
-	#Computes the Covariance based estimator of D, localization sigma, variance of D, Signal to noise, and the power spectrum of dx
-	# For details, see Vestergaard (2014)
-	# Note that R is hard coded to 1/6, which should be the usual value
+	"""Computes the covariance-based estimator for D and localizationVariance, as well as variance in D, SNR, and Pxk
+	
+	Keyword arguments:
+	x: a 1Dnumpy array of particle track data in pixels
+	dt: the time step of data
+	scale: um per pixel for x
+
+	Returns:
+	[D, localizationVariance, varianceD, SNR], Pxk
+	"""
+	# For details, see [1]
+	# Note that R is hard coded to 1/6, which should be the usual value for a shutter that is kept open during the entire time lapse [1]
 	R = 1./6
 	N = x.size
-	dx = np.diff(x) #(has N - 1 elements)
+	dx = np.diff(scale*x) #(has N - 1 elements)
 
 	#An array of elements (deltaX_n)*(deltaX_n-1) (should have N - 2 elements so just do the calculation and then truncate)
 	dxn_dxn1 = dx*np.roll(dx,-1)
 	dxn_dxn1 = dxn_dxn1[0:-1]
+
+	# If the localization variance is known, D can be computed using eq [1].16, which also changes the calculation for the variance in D.
+	# This function does not currently support that functionality.
     
 	D = np.mean(dx**2)/2/dt + np.mean(dxn_dxn1)/dt
 	localizationVariance = R*np.mean(dx**2) + (2*R - 1)*np.mean(dxn_dxn1)
@@ -38,54 +65,87 @@ def cveD(x, dt, scale):
 	return np.array([D, localizationVariance, varianceD, SNR]), Pxk
 
 def theoryP(D, dt, localizationVariance, N):
-    #For a given D, dt, and sigma**2, returns equation (10) from Vestergaard with N points. There might be a discrepency in N:
-    # there are technically N - 1 values of k since it is indexing deltaX
-    k = np.arange(0,N)
-    #Hard code R as 1/6
-    R = 1./6
-    #Note that R is hard coded here as 1/6
-    return 2*D*dt**2 + 2*(localizationVariance*dt - 2*D*R*dt**2)*(1 - np.cos(pi*k/(N+1)))
+	"""Computes the theoretical power spectrum for a freely diffusing particle (eq 10 in [1])
+
+	Keyword arguments:
+	D: diffusion constant
+	dt: time step
+	localizationVariance: 
+	N: total number of time steps to generate
+
+	Returns:
+	theoryPxk: theoretical power spectrum of a freely diffusing particle
+	"""
+	# There might be a discrepency in N:
+	# there are technically N - 1 values of k since it is indexing deltaX
+	k = np.arange(0,N)
+	#Hard code R as 1/6
+	R = 1./6
+	#Note that R is hard coded here as 1/6
+	return 2*D*dt**2 + 2*(localizationVariance*dt - 2*D*R*dt**2)*(1 - np.cos(pi*k/(N+1)))
 
 def boxAv(x, boxSize):
-    N = x.size
-    numBoxes = np.ceil(N/boxSize)
-    lastBox = np.mod(N,boxSize)
-    binAv = np.zeros((numBoxes))
-    for i in np.arange(0,numBoxes - 1):
-        binAv[i] = np.mean(x[i*boxSize:(i+1)*boxSize - 1])
+	"""Downsamples x with boxSize points per bin"""
+	N = x.size
+	numBoxes = np.ceil(N/boxSize)
+	lastBox = np.mod(N,boxSize)
+	binAv = np.zeros((numBoxes))
+	for i in np.arange(0,numBoxes - 1):
+		binAv[i] = np.mean(x[i*boxSize:(i+1)*boxSize - 1])
     
-    if lastBox ==1:
-        binAv[-1] = x[-1]
-    else:
-        binAv[-1] = np.mean(x[-1:-(lastBox):-1])
-    return binAv
+	if lastBox ==1:
+		binAv[-1] = x[-1]
+	else:
+		binAv[-1] = np.mean(x[-1:-(lastBox):-1])
+	return binAv
 
 def gammaChiSquared(n, bins):
-    # Calculate the X**2 test statistic of calculated Pxk histogram values to theory Pxk = 2D(dt**2) + 2(sigma**2dt - 2DR(dt**2)(1-cos(pik/(N+1))))
-    # input n should be a pdf from plt.hist(... normed=True) such that sum(n*binwidth) = 1
-    # 
-    binwidth = np.diff(bins)[0]
+	"""Calculates the chi^2 test statistic and p value against a gamma(1/2,2) distribution
+	
+	Keyword arguments:
+	n: pdf values of a histogram (as from pyplot.hist(x,N,normed=True)
+	bins: bins locations
+
+	Returns:
+	p: p value
+	X2: reduced chi^2 value
+	"""
+	# Useful for testing free diffusion
+	# input n should be a pdf from plt.hist(... normed=True) such that sum(n*binwidth) = 1
+	#
+	grain = 1000.
+	binwidth = np.diff(bins)[0]
     
-    bins2 = binwidth/100.*(1+np.arange(0,100*n.size))
+	bins2 = binwidth/grain*(1+np.arange(0,grain*n.size))
     
-    temp = scipy.stats.gamma.pdf(bins2,0.5, scale=2)
-    temp = np.reshape(temp,(n.size,100))*binwidth/100
-    expected = np.reshape(np.sum(temp,1),(n.size))
+	temp = scipy.stats.gamma.pdf(bins2,0.5, scale=2)
+	temp = np.reshape(temp,(n.size,grain))*binwidth/grain
+	expected = np.reshape(np.sum(temp,1),(n.size))
         
-    observed = n*binwidth
-    plt.bar(bins[0:-1],observed,binwidth)
-    plt.plot(binwidth/2 + bins[0:-1],expected,'r', lw=5)
+	observed = n*binwidth
+	plt.bar(bins[0:-1],observed,binwidth)
+	plt.plot(binwidth/2 + bins[0:-1],expected,'r', lw=5)
     
-    p, X2 = scipy.stats.chisquare(observed, expected, 0)
-    #temp = (observed - expected)**2/expected
-    #X2 = np.sum(temp)
-    return p, X2
+	p, X2 = scipy.stats.chisquare(observed, expected, 0)
+	#temp = (observed - expected)**2/expected
+	#X2 = np.sum(temp)
+	return p, X2
     
     
 def D2eta(D, R, T):
-    #Takes D in um**2/s, R in um, T in C, and returns eta in centipoise
-    k = scipy.constants.k
-    T = scipy.constants.C2K(T)
-    R = R*scipy.constants.micro
-    D = D*scipy.constants.micro**2
-    return k*T/6/pi/R/D*1000
+	"""Computes the viscosity of a fluid in Pa*s
+
+	Keyword arguments:
+	D: diffusion constant in um^2/s
+	R: tracked particle radius in um
+	T: temperature in C
+
+	Returns:
+	eta: viscosity in Pa*s (multiply by 1000 for cP)
+	"""
+	#Takes D in um**2/s, R in um, T in C, and returns eta in centipoise
+	k = scipy.constants.k
+	T = scipy.constants.C2K(T)
+	R = R*scipy.constants.micro
+	D = D*scipy.constants.micro**2
+	return k*T/6/pi/R/D*1000
